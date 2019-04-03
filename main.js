7917a72f3d13931e03e4655b730f10f49c56b050
Scott Fasone <scott181182@yahoo.com>
2019-04-03 19:36:09 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,81 @@
+const Vue = require ('vue');
+const axios = require('axios');
+
+const context = new AudioContext();
+
+
+Vue.component('sound-box', {
+    data: () => {
+        return {
+            empty: true,
+            loading: false,
+            editing: false,
+            yturl: '',
+            title: '',
+            buffer: undefined,
+            players: 0
+        };
+    },
+    computed: {
+        vid: function() { return new URL(this.yturl).searchParams.get('v'); },
+        playing: function() { return this.players > 0; }
+    },
+    methods: {
+        load_sound: function()
+        {
+            this.loading = true;
+            console.log(`Fetching '${this.vid}'`);
+
+            axios.get(`/api/info/${this.vid}`)
+                .then(res => {
+                    const data = res.data.data;
+                    this.title = data.title;
+
+                    return axios({
+                        method: 'get',
+                        url: `/api/audio/${this.vid}`,
+                        headers: { 'Content-Type': 'audio/mpeg' },
+                        responseType: 'arraybuffer'
+                    });
+                })
+                .then(res => context.decodeAudioData(res.data))
+                .then(buffer => {
+                    this.buffer = buffer;
+                    this.loading = false;
+                    this.empty = false;
+                })
+                .catch(err => { console.error(err); });
+        },
+        play_sound: function()
+        {
+            if(!this.buffer) { return console.log('No sound loaded!'); }
+
+            const source = context.createBufferSource();
+            source.buffer = this.buffer;
+            source.connect(context.destination);
+            source.onended = () => {
+                this.players--;
+                console.log(this.players);
+            };
+            source.start();
+            this.players++;
+            console.log(this.players);
+        },
+        clear_sound: function()
+        {
+            this.title = '';
+            this.yturl = '';
+            this.loading = false;
+            this.empty = true;
+            this.buffer = undefined;
+        }
+    },
+    template: '#sound-box-template'
+});
+
+const app = new Vue({
+    el: '#soundboard',
+    data: {
+        tile_count: 16
+    }
+});
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+
+const app = express();
+const port = 8080;
+
+app.get('/api/info/:vid', (req, res) => {
+	const vid = req.params.vid;
+	console.log(`Fetching vid '${vid}'...`);
+	ytdl.getBasicInfo(vid, (err, info) => {
+		if(err) {
+			console.error(`Error on fetch '${vid}'.`);
+			return res.status(500).send({ status: "error", error: err });
+		}
+		console.log(`Fetched ${vid}!`);
+		const data = {
+			title: info.title,
+			thumbnail: info.thumbnail_url,
+			vid: info.video_id
+		}
+		res.send({ status: "ok", data });
+	});
+});
+
+app.get('/api/audio/:vid', (req, res) => {
+	const vid = req.params.vid;
+	const yturl = `https://www.youtube.com/watch?v=${vid}`;
+
+	res.type('audio/mpeg');
+	ytdl(yturl, { quality: 'highestaudio', filter: 'audioonly' }).pipe(res);
+});
+
+app.use(express.static('public'));
+app.listen(port, () => {
+	console.log(`Listening on port ${port}!`);
+});
